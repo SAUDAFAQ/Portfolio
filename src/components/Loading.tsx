179f39610f3776b3b90103a0213dab377b59ca94
SAUD AFAQ <saud.afaq123@gmail.com>
2026-03-30 04:14:44 +0200

    udpate loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./styles/Loading.css";
 import { useLoading } from "../context/LoadingProvider";
+import { profile } from "../data/portfolio";
 
 import Marquee from "react-fast-marquee";
 
@@ -46,7 +47,7 @@ const Loading = ({ percent }: { percent: number }) => {
     <>
       <div className="loading-header">
         <a href="/#" className="loader-title" data-cursor="disable">
-          AM
+          {profile.initials}
         </a>
         <div className={`loaderGame ${clicked && "loader-out"}`}>
           <div className="loaderGame-container">
@@ -62,8 +63,9 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
+            <span> Senior Flutter engineer</span>
+            <span> Mobile developer</span>
+            <span> {profile.name}</span>
           </Marquee>
         </div>
         <div
